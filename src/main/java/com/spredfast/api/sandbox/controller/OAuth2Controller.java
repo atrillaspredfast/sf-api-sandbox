9b2458557ec9b8e1cd1773ad061a9e4ad11405e2
atrilla <atrilla@spredfast.com>
2016-01-21 17:49:37 -0300

    HW2016-13: OAuth integration - accessToken

--- src/main/java/com/spredfast/api/sandbox/controller/OAuth2Controller.java
+++ src/main/java/com/spredfast/api/sandbox/controller/OAuth2Controller.java
@@ -1,5 +1,7 @@
 package com.spredfast.api.sandbox.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import com.spredfast.api.sandbox.dao.ClientRepository;
 import com.spredfast.api.sandbox.dao.EnvironmentRepository;
+import com.spredfast.api.sandbox.domain.Client;
 import com.spredfast.api.sandbox.service.ISfApiDefinitionService;
 
 @RestController
@@ -28,7 +31,18 @@ public class OAuth2Controller {
 	public String fetchDefinition(@RequestParam(name = "state") String state,
 			@RequestParam(name = "code") String code) {
 		RestTemplate restTemplate = new RestTemplate();
-		TokenResponse tokenResponse = restTemplate.getForObject(sessionData.getTokenURL(), TokenResponse.class);
+		Map<String, String> params = new HashMap<>();
+		Client client = clientRepository.findAll().iterator().next();
+
+		params.put("client_id", client.getAuthClientId());
+		params.put("client_secret", client.getAuthSecret());
+		//FIXME : add SessionData.uriRedirect
+//		params.put("redirect_uri", sessionData.getUriRedirect());
+		params.put("code", code);
+
+		// FIXME : add post method call
+//		TokenResponse tokenResponse = restTemplate.postForObject(sessionData.getTokenURL(), TokenResponse.class, params);
+		TokenResponse tokenResponse = null;
 
 		if (tokenResponse.isSuccess() && tokenResponse.getAccessToken().isPresent()) {
 			sessionData.setAccecssToken(tokenResponse.getAccessToken().get());
